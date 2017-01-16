a5e0f1a84e3974520a6852063427bf522dac4df5
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2017-01-15 23:41:52 -0800

    Add home page component: generate random url, createPoll, redirect page to poll url

--- components/home/home.js
+++ components/home/home.js
@@ -13,21 +13,23 @@ pollster.controller('HomeCtrl', ['$scope', '$location', function($scope, $locati
 
   $scope.createPoll = function() {
     var url = generateUrl();
-    var test = firebase.database().ref().child('polls').child(url);
+    var test = firebase.database().ref().child('polls');
 
-    test.on('value', function(snap) {
-      if (snap.val()) {
+    test.once('value', function(snap) {
+      if (snap.hasChild(url)) {
+        debugger;
+        // this url exists, re-generateUrl
+        $scope.createPoll();
+      } else {
         debugger;
         firebase.database().ref('polls/' + url).set({
           newPoll: true
-        });
-      } else {
-        // this url exists, re-generateUrl
-        debugger;
-        $scope.createPoll();
+        }).then(function() {
+          console.log('url created successfully');
+          debugger;
+          $location.path('/' + url);
+        })
       }
-
-      $location.path('/' + url);
     });
   };
 
